312b7e91ebed65f6a4c1f7f90743ff82a91577cf
i99dev <obaid.altuniji@gmail.com>
2022-06-22 16:40:32 +0400

    commit init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["content/**/*.md", "layouts/**/*.html"],
+  theme: {
+    container: {
+        center: true,
+      },
+    extend: {},
+  },
+  plugins: [require('tailwindcss-rtl')],
+}
